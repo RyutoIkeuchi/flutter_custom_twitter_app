6d10e6b4ba76002e91900f0a94949bccdf733ffa
ryuto <ryu.i7133@gmail.com>
2021-09-05 15:18:58 +0900

    ディレクトリ構成を作成、ホーム画面を作成

---
+++ lib/ui/pages/Home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      home: Scaffold(
+        appBar: AppBar(
+        title:Text('Home')
+         ),
+      body: Center(
+        child: Column(
+          children:<Widget> [
+            Text('Homeページです')
+          ]
+        ),
+      ),
+    )
+    );
+  }
+
+}
