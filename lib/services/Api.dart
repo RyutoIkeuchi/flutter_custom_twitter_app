75b8fd572b3b59ab7637386490019d1c05be358e
ryuto <ryu.i7133@gmail.com>
2021-10-10 10:56:22 +0900

    タイムライン取得のAPI処理を別に分けた

--- lib/services/Api.dart
+++ lib/services/Api.dart
@@ -1,72 +1,39 @@
-import 'dart:convert';
-import 'package:flutter/material.dart';
 import 'package:oauth1/oauth1.dart' as oauth1;
-import 'package:url_launcher/url_launcher.dart';
+import '../../../config.dart';
+import 'dart:convert';
 
-import '../config.dart';
+final platform = oauth1.Platform(
+  'https://api.twitter.com/oauth/request_token',
+  'https://api.twitter.com/oauth/authorize',
+  'https://api.twitter.com/oauth/access_token',
+  oauth1.SignatureMethods.hmacSha1,
+);
 
-class ApiToken extends StatefulWidget {
-  @override
-  _ApiTokenState createState() => _ApiTokenState();
-}
+final clientCredentials = oauth1.ClientCredentials(
+  TWITTER_API_KEY,
+  TWITTER_API_KEY_SECRET,
+);
 
-class _ApiTokenState extends State<ApiToken> {
-  final controller = TextEditingController();
-  final platform = oauth1.Platform(
-    'https://api.twitter.com/oauth/request_token',
-    'https://api.twitter.com/oauth/authorize',
-    'https://api.twitter.com/oauth/access_token',
-    oauth1.SignatureMethods.hmacSha1,
+late final auth = oauth1.Authorization(clientCredentials, platform);
+oauth1.Credentials? tokenCredentials;
+
+Future<dynamic> getApi() async {
+  final client = oauth1.Client(
+    platform.signatureMethod,
+    clientCredentials,
+    oauth1.Credentials(
+      ACCESS_TOKEN,
+      ACCESS_TOKEN_SECRET,
+    ),
   );
-  final clientCredentials = oauth1.ClientCredentials(
-    TWITTER_API_KEY,
-    TWITTER_API_KEY_SECRET,
+
+  final res = await client.get(
+    Uri.https(
+      'api.twitter.com',
+      '/1.1/statuses/home_timeline.json',
+      {'count': '100'},
+    ),
   );
-  late final auth = oauth1.Authorization(clientCredentials, platform);
-  oauth1.Credentials? tokenCredentials;
 
-  @override
-  void initState() {
-    super.initState();
-
-    auth.requestTemporaryCredentials('oob').then((res) {
-      tokenCredentials = res.credentials;
-
-      launch(auth.getResourceOwnerAuthorizationURI(tokenCredentials!.token));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      TextFormField(
-        controller: controller,
-      ),
-      ElevatedButton(
-        onPressed: () async {
-          final pin = controller.text;
-          final verifier = pin;
-          final res =
-              await auth.requestTokenCredentials(tokenCredentials!, verifier);
-          print('Access Token: ${res.credentials.token}');
-          print('Access Token Secret: ${res.credentials.tokenSecret}');
-
-          final client = oauth1.Client(
-            platform.signatureMethod,
-            clientCredentials,
-            res.credentials,
-          );
-
-          final apiResponse = await client.get(
-            Uri.parse('https://api.twitter.com/1.1/statuses/home_timeline.json?count=1')
-          );
-          print(apiResponse.body);
-        },
-        child: Text('タップ'),
-      )
-    ])));
-  }
+  return jsonDecode(res.body);
 }
--- lib/ui/pages/TimelinePage.dart
+++ lib/ui/pages/TimelinePage.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_twitter_app/ui/components/templates/DrawerView.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import "package:intl/intl.dart";
-import 'package:oauth1/oauth1.dart' as oauth1;
 
-import '../../../config.dart';
+import '../../services/Api.dart';
 import '../components/templates/DrawerView.dart';
 
 class Timeline extends StatefulWidget {
@@ -87,51 +84,10 @@ class _Timeline extends State<Timeline> {
     }
   }
 
-  final controller = TextEditingController();
-  final platform = oauth1.Platform(
-    'https://api.twitter.com/oauth/request_token',
-    'https://api.twitter.com/oauth/authorize',
-    'https://api.twitter.com/oauth/access_token',
-    oauth1.SignatureMethods.hmacSha1,
-  );
-
-  final clientCredentials = oauth1.ClientCredentials(
-    TWITTER_API_KEY,
-    TWITTER_API_KEY_SECRET,
-  );
-
-  late final auth = oauth1.Authorization(clientCredentials, platform);
-  oauth1.Credentials? tokenCredentials;
-
   @override
-  void initState() {
+  void initState() async {
     super.initState();
-    getApi();
-  }
-
-  Future<dynamic> getApi() async {
-    // OauthToken();
-    final client = oauth1.Client(
-      platform.signatureMethod,
-      clientCredentials,
-      oauth1.Credentials(
-        ACCESS_TOKEN,
-        ACCESS_TOKEN_SECRET,
-      ),
-    );
-    final res = await client.get(
-      Uri.https(
-        'api.twitter.com',
-        '/1.1/statuses/home_timeline.json',
-        {'count': '100'},
-      ),
-    );
-    dynamic body = jsonDecode(res.body);
-
-    dynamic data = body;
-    setState(() {
-      _data = data;
-    });
+    _data = await getApi();
   }
 
   @override
