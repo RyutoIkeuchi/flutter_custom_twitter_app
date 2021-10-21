88c79be290c981c92cd5120cb3ea659f86f495f2
ryuto <ryu.i7133@gmail.com>
2021-10-21 21:16:26 +0900

    検索画面の検索フォームを作成

--- lib/ui/pages/search_page.dart
+++ lib/ui/pages/search_page.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_custom_twitter_app/models/home_timeline_model.dart';
 import 'package:flutter_custom_twitter_app/services/home_time_line.dart';
 import 'package:flutter_custom_twitter_app/services/search_tweet_api.dart';
@@ -40,40 +41,79 @@ class Search extends StatelessWidget {
     }
   }
 
+  // void _handleText(String e) {
+  //   setState(() {
+  //     _text = e;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<TweetTimelineData>(
-        create: (_) => TweetTimelineData()..getTweetTimelineData(),
-        child: Scaffold(
-            appBar: AppBar(
-              title: FaIcon(FontAwesomeIcons.twitter),
-            ),
-            drawer: DrawerView(),
-            body: Consumer<TweetTimelineData>(builder: (context, model, child) {
-              List<dynamic> data = model.data;
-              if (data == []) {
-                return CircularProgressIndicator();
-              }
-              return RefreshIndicator(
-                onRefresh: () async {
-                  print('リフレッシュします');
-                  await TweetTimelineData().refresh();
-                },
-                child: ListView.builder(
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    itemCount: data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(
-                                      color: Colors.black12, width: 1.0))),
-                          child: !checkTextData(data[index]['text'])
-                              ? tweetCard(data[index])
-                              : reTweetCard(data[index]));
-                    }),
-              );
-            })));
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          width: 260,
+          height: 40,
+          // padding:EdgeInsets.only(right: 10, bottom: 30, left: 10),s
+          child:TextField(
+             style: TextStyle(
+                height: 0.9,
+                fontSize: 15,              
+             ),
+            decoration: InputDecoration(
+              // border: InputBorder.none,
+              border:OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
+              fillColor: Colors.black12,
+              filled: true,
+              prefixIcon:Icon(Icons.search,size: 20),
+              contentPadding: EdgeInsets.all(10.0),
+              hintText: 'キーワード検索'
+            ),        
+          ),
+        ),
+        leading:IconButton(
+          icon: CircleAvatar(
+            child: Icon(Icons.people),
+            backgroundColor: Colors.red,
+            radius: 16,
+          ),
+          onPressed: () {
+
+          },
+        ),
+      ), 
+      body:searchTweet(),
+    );
   }
+}
+
+Widget searchTweet() {
+  return ChangeNotifierProvider<TweetTimelineData>(
+      create: (_) => TweetTimelineData()..getTweetTimelineData(),
+      child: Consumer<TweetTimelineData>(builder: (context, model, child) {
+        List<dynamic> data = model.data;
+        if (data == []) {
+          return CircularProgressIndicator();
+        }
+        return RefreshIndicator(
+            onRefresh: () async {
+              print('リフレッシュします');
+              await TweetTimelineData().refresh();
+            },
+            child:ListView.builder(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  itemCount: data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                                    color: Colors.black12, width: 1.0))),
+                        child: !checkTextData(data[index]['text'])
+                            ? tweetCard(data[index])
+                            : reTweetCard(data[index]));
+                  }),
+            );
+      }));
 }
